19ccf6f61ab8e0389f219499f6a5d8911cb3e31b
eferrin <eferrin_93@hotmail.com>
2016-12-13 12:01:30 -0300

    centros harcodeados

--- controllers/centerController.js
+++ controllers/centerController.js
@@ -21,12 +21,29 @@ module.exports = function(app){
     centerDao.updateCenter(req.params.id, req.body);
     res.writeHead(200);
     res.end('center updated');
-  })
+  }),
   app.delete('/center/:id', function(req, res){
     centerDao.delete(req.params.id);
     res.writeHead(200);
     res.end('center updated');
-  })
+  }),
+  // hardcode data
+  app.get('/loadCenters', function(req,res){
+    for (var i=0;i<5;i++){
+      var center= new Center({
+        name:'a'+i;
+        numberOfPeople: 0;
+        capacity:Math.floor((Math.random() * 1000) + 1);
+        direction:'abc'+i;
+        lat: Math.floor((Math.random()*180)-90);
+        long: Math.floor((Math.random()*360)-180);
+      });
+      CenterDao.insertCenter(center);
+    }
+    res.writeHead(200);
+    res.end('centers added');
+
+  },
 
 
 
