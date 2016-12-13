b0cf7bf638da3f45c91d1dc231a7343226eee1eb
eferrin <eferrin_93@hotmail.com>
2016-12-13 12:13:14 -0300

    manejo errores controller

--- controllers/centerController.js
+++ controllers/centerController.js
@@ -1,31 +1,61 @@
 var centersDao = require('../dao/CenterDao');
 
 module.exports = function(app){
-    //app.get - post de usuarios
 
   app.get('/centers', function(req,res){
 
-    res.send(centerDao.getAll());
+    centers=centerDao.getAll());
 
+    if (centers == 'undefined'){
+      res.writeHead(404);
+      res.end('centers not found')
+    } else {
+      res.writeHead(200);
+      res.send(center);
+    }
   },
   app.get('/center/:id', function(req, res){
     var id = req.params.id;
-    res.send(centerDao.getCenter(id));
+    var center=centerDao.getCenter(id);
+    if (center == 'undefined'){
+      res.writeHead(404);
+      res.end('center not found')
+    } else {
+      res.writeHead(200);
+      res.send(center);
+    }
   }),
   app.post('/center', function(req, res){
     centerDao.insertCenter(req.body);
-    res.writeHead(200);
-    res.end('center added');
+    if (err){
+      res.writeHead(404);
+      res.end('center not added')
+
+    }else{
+      res.writeHead(200);
+      res.end('center added');
+    }
   }),
   app.put('/center/:id', function(req,res){
     centerDao.updateCenter(req.params.id, req.body);
-    res.writeHead(200);
-    res.end('center updated');
+    if (err){
+      res.writeHead(404);
+      res.end('center not updated')
+
+    }else{
+      res.writeHead(200);
+      res.end('center updated');
+    }
   }),
   app.delete('/center/:id', function(req, res){
     centerDao.delete(req.params.id);
-    res.writeHead(200);
-    res.end('center updated');
+    if (err){
+      res.writeHead(404);
+      res.end('center not deleted')
+    }else{
+      res.writeHead(200);
+      res.end('center deleted');
+    }
   }),
   // hardcode data
   app.get('/loadCenters', function(req,res){
